f1f8625a37763acf3dee69085f70d8babac780e0
Sumarmahadev <sumarchimacode@gmail.com>
2025-07-20 18:25:09 +0530

    Update miniprojectphase.py

--- miniprojectphase.py
+++ miniprojectphase.py
@@ -1,16 +1,15 @@
 import cv2 
 
-# Step 1: Ask user for image file name
+# Step 1: Ask user for image file name or load the image by user input
 user = input("Enter the image file name (e.g. image.jpg): ")
 image = cv2.imread(user)
 
 # Step 2: Check if image is found
 if image is not None:
-    # Step 3: Convert to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Step 3: Convert to grayscale
 
-    # Step 4: Show the grayscale image
-    cv2.imshow("Grayscale Image", gray)
+    
+    cv2.imshow("Grayscale Image", gray) # Step 4: Show the grayscale image
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -19,9 +18,9 @@ if image is not None:
     
     if save_options == "yes":
         output_name = input("Enter the output filename (e.g. output.jpg): ")
-        cv2.imwrite(output_name, gray)  # ✅ This saves the grayscale image
+        cv2.imwrite(output_name, gray)  #here we saves the as as user name with that coverted  image
         print(f"Image saved as {output_name}")
     else:
         print("Image not saved.")
 else:
-    print("❌ Error: Image not found. Please check the file name or path.")
+    print("Error: Image not found. Please check the file name or path.")
